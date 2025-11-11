72fcc31bc68af2573b8bdd20c86fe460365a4e5a
AnjiniKatari <139158845+AnjiniKatari@users.noreply.github.com>
2025-11-10 20:01:30 -0500

    first pods i assignment

---
+++ project 0/streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+#goal of streamlit is to just work on the backend, streamlit will take care of the front end 
+
+st.title("Title")
+
+st.code("import pandas as pd" "\n"
+        "dataaaaa")
+
+st.image("cat.jpg")
+
+#can add interactive things -> checkbox, button, radio, etc 
